fccc72d8f88bd7e285113eba74a5f9eecccaa15e
Jérôme Piquot <jpiquot@itaneo.com>
2026-04-30 18:01:20 +0200

    test: serialize integration test assembly

---
+++ tests/Hexalith.Memories.IntegrationTests/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// <copyright file="AssemblyInfo.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
